15c5fe70f749e391add5c0631219fa27ea70c670
Krissy Dukes <krissydotnet@yahoo.com>
2018-10-15 18:18:19 -0400

    Updates to leaflet
    Loaded new test data.  Changed the map icon to be twitter logo .

--- javascript/mapscript.js
+++ javascript/mapscript.js
@@ -3,8 +3,6 @@
 var mapCenterCoordinates = [39.8333333, -98.585522];
 var zoomLevel = 4;
 var maxZoomLevel = 19;
-// var mapLocation = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-// var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
 var mapLocation = 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png';
 var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
@@ -29,17 +27,18 @@ loadGeoJSONData();
 
 
 // FUNCTIONS //
-
 function buildMarkers(feature, layer) {
     //Setup Map Markers
-    var circleIcon = L.divIcon({ className : 'circle',
-    iconSize : [ 15, 15 ]});
+    var circleIcon = L.divIcon({ className : 'circle fab fa-twitter',
+    iconSize : [ 20, 20 ]});
     layer.setIcon(circleIcon);
 
     //Setup PopUp
-    layer.bindPopup('<div class="tweet-info"><div class="tweet-img"><img src="https://pbs.twimg.com/profile_images/1039147701244325889/6qGpmATq_400x400.jpg"></div>' +
-                    '<div class="tweet-name">' + feature.properties.RegionName + '</div>' +
-                    '<div class="tweet-text">' + feature.properties.tweets + '</div>' +
+    layer.bindPopup('<div class="tweet-info"><div class="tweet-img">' +
+                    '<img src="' + feature.properties.image_url + '"></div>' +
+                    '<div class="tweet-name">' + feature.properties.s_name + '</div>' +
+                    '<div class="tweet-text">' + cleanText(feature.properties.t_text) + '</div>' +
+                    '<div class="tweet-date">' + feature.properties.t_date + '</div>' +
                     '</div>');
   
 }
@@ -50,5 +49,9 @@ function loadGeoJSONData() {
     L.geoJson(test_data, {
         onEachFeature: buildMarkers
     }).addTo(map);  
+}
+function cleanText(txtString) {
+    var regEx = /[^\w\s:\/#]/gi;
+    return txtString.replace(regEx, '');
 }
 
